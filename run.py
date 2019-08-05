ce073e57b89fb766257e46f3c1a3fac089ac8c6a
Gaurav Bhagwanani <gaurav.bhagwanani@gmail.com>
2019-08-06 01:17:58 +0530

    added checkout page

--- run.py
+++ run.py
@@ -6,9 +6,21 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
-@app.route('/flights')
-def flights_page():
+@app.route('/flights-display')
+def show_flights():
     return render_template('flights.html')
+
+@app.route('/flights')
+def find_flights():
+    return render_template('search-flights.html')
+
+@app.route('/checkout')
+def checkout():
+    return render_template('booking.html')
+
+@app.errorhandler(404)
+def error_handle(error):
+    return 'Page not found'
 
 if __name__ == '__main__':
     app.run(debug = True)
